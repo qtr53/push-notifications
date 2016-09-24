47ef0e328bed9a08f61a55a605515014f7aeb7d3
qtr53 <q77772004@gmail.com>
2016-09-24 03:52:10 +0300

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // TODO
+if ('serviceWorker' in navigator) {
+  console.log('Service Worker is supported');
+  navigator.serviceWorker.register('sw.js').then(function() {
+    return navigator.serviceWorker.ready;
+  }).then(function(reg) {
+    console.log('Service Worker is ready :^)', reg);
+      // TODO
+  }).catch(function(error) {
+    console.log('Service Worker error :^(', error);
+  });
+}
+
